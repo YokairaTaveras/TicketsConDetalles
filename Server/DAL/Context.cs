e3477afa6a3616245984b16c1f874cd427b5832d
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-24 14:43:13 -0400

    Creando el contexto

---
+++ Server/DAL/Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TicketsConDetalles.Server.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<TicketsDetalle> TicketsDetalle { get; set; }
+
+    }
+}
