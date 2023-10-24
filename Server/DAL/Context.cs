8922725bd79442cb37b252f5e35f933a02882d8a
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-24 15:17:05 -0400

    Creando el modelo de TicketsDetalle

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace TicketsConDetalles.Server.DAL
 {
---
+++ Shared/Models/TicketsDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsConDetalles.Shared.Models
+{
+    internal class TicketsDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        [ForeignKey("TicketId")]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [StringLength(35, ErrorMessage = "El Emisor es muy largo. Excede los {0} caracteres")]
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "El campo Emisor solo debe de contener caracteres alfabeticos")]
+        public string Emisor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo Mensaje es obligatorio")]
+        [StringLength(75, ErrorMessage = "Has ingresado mas de {0} caracteres")]
+        public string Mensaje { get; set; } = string.Empty;
+    }
+}
